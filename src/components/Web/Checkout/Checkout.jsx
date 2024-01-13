127d426c80df894daf0580e39402a8614fd3b382
lama saad <lamasaad65@gmail.com>
2024-01-13 12:20:01 +0300

    Add inputs to checkout.

--- src/components/Web/Checkout/Checkout.jsx
+++ src/components/Web/Checkout/Checkout.jsx
@@ -1,6 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { CartContext } from '../Context/Cart'
+import { useFormik } from 'formik'
+import Input from '../pages/Input'
 
 export default function Checkout() {
+
+    const { getCartContext, total } = useContext(CartContext)
+    const [products, setProducts] = useState([])
+
+    const getCart = async () => {
+        const res = await getCartContext()
+        setProducts(res.products)
+    }
+
+    useEffect(() => {
+        getCart()
+    }, [])
+
+    const initialValues = { couponName: "", address: "", phone: "" }
+
+    const onSubmit = (order)=>{
+        console.log(order)
+    }
+
+    const formik = useFormik({ initialValues,onSubmit, validationSchema: })
+    const inputs = [
+        {
+            id: "address",
+            name: "address",
+            placeHolder: "enter your address",
+            type: "text",
+            value: formik.values.address
+        },
+        {
+            id: "phone",
+            name: "phone",
+            placeHolder: "enter your phone",
+            type: "text",
+            value: formik.values.phone
+        },
+        {
+            id: "couponName",
+            name: "couponName",
+            placeHolder: "enter your couponName",
+            type: "text",
+            value: formik.values.couponName
+        }
+    ]
+
+    const renderInputs = inputs.map((input, index) =>
+        <Input
+            key={index}
+            input={input}
+            errors={formik.errors}
+            onChange={input.onChange || formik.handleChange}
+            touched={formik.touched}
+            onBlur={formik.handleBlur}
+        />
+    )
+
+
     return (
         <>
             <div className="container-fluid pt-5" >
@@ -9,18 +68,13 @@ export default function Checkout() {
                         <div className="mb-4" >
                             <h4 className="font-weight-semi-bold mb-4" >Billing Address</h4>
                             <div className="row" >
-                                <div className="col-md-6 form-group" >
-                                    <label>Mobile No</label>
-                                    <input class="form-control" type="text" placeHolder="+123 456 789" />
-                                </div>
-                                <div className="col-md-6 form-group" >
-                                    <label>Address</label>
-                                    <input class="form-control" type="text" placeHolder="123 Street" />
-                                </div>
-                                <div className="col-md-6 form-group" >
-                                    <label>Coupon Name</label>
-                                    <input class="form-control" type="text" placeHolder="coupon name" />
-                                </div>
+                                <form onSubmit={formik.handleSubmit}>
+                                    {renderInputs}
+                                    <div className="border-secondary bg-transparent" >
+                                        <button type='submit' className="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3" >Place Order</button>
+                                    </div>
+
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -31,61 +85,29 @@ export default function Checkout() {
                             </div>
                             <div className="card-body" >
                                 <h5 className="font-weight-medium mb-3" >Products</h5>
-                                <div className="d-flex justify-content-between" >
-                                    <p>Colorful Stylish Shirt 1</p>
-                                    <p>$150</p>
-                                </div>
-                                <div className="d-flex justify-content-between" >
-                                    <p>Colorful Stylish Shirt 2</p>
-                                    <p>$150</p>
-                                </div>
-                                <div className="d-flex justify-content-between" >
-                                    <p>Colorful Stylish Shirt 3</p>
-                                    <p>$150</p>
-                                </div>
+                                {products ?
+                                    products.map(prod => (
+                                        <div className="d-flex justify-content-between" >
+                                            <p>{prod.details.name}  {prod.quantity}x</p>
+                                            <p>${prod.details.finalPrice}</p>
+                                        </div>
+                                    ))
+                                    : "isLoading ..."}
                                 <hr className="mt-0" />
-                                    <div className="d-flex justify-content-between mb-3 pt-1" >
-                                        <h6 className="font-weight-medium" >Subtotal</h6>
-                                        <h6 className="font-weight-medium" >$150</h6>
-                                    </div>
-                                    <div className="d-flex justify-content-between" >
-                                        <h6 className="font-weight-medium" >Shipping</h6>
-                                        <h6 className="font-weight-medium" >$10</h6>
-                                    </div>
+                                <div className="d-flex justify-content-between mb-3 pt-1" >
+                                    <h6 className="font-weight-medium" >Subtotal</h6>
+                                    <h6 className="font-weight-medium" >${total}</h6>
+                                </div>
+                                <div className="d-flex justify-content-between" >
+                                    <h6 className="font-weight-medium" >Shipping</h6>
+                                    <h6 className="font-weight-medium" >$0</h6>
+                                </div>
                             </div>
                             <div className="card-footer border-secondary bg-transparent" >
                                 <div className="d-flex justify-content-between mt-2" >
                                     <h5 className="font-weight-bold" >Total</h5>
-                                    <h5 className="font-weight-bold" >$160</h5>
+                                    <h5 className="font-weight-bold" >${total}</h5>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="card border-secondary mb-5" >
-                            <div className="card-header bg-secondary border-0" >
-                                <h4 className="font-weight-semi-bold m-0" >Payment</h4>
-                            </div>
-                            <div className="card-body" >
-                                <div className="form-group" >
-                                    <div className="custom-control custom-radio" >
-                                        <input type="radio" class="custom-control-input" name="payment" id="paypal" />
-                                            <label class="custom-control-label" htmlFor="paypal" >Paypal</label>
-                                    </div>
-                                </div>
-                                <div className="form-group" >
-                                    <div className="custom-control custom-radio" >
-                                        <input type="radio" class="custom-control-input" name="payment" id="directcheck" />
-                                            <label class="custom-control-label" htmlFor="directcheck" >Direct Check</label>
-                                    </div>
-                                </div>
-                                <div className="" >
-                                    <div className="custom-control custom-radio" >
-                                        <input type="radio" class="custom-control-input" name="payment" id="banktransfer" />
-                                            <label class="custom-control-label" htmlFor="banktransfer" >Bank Transfer</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-footer border-secondary bg-transparent" >
-                                <button className="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3" >Place Order</button>
                             </div>
                         </div>
                     </div>
